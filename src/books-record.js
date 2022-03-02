3cdfeec79625d648321d8f945685eec8cb86f8d9
Jyoti Malhotra <92352691+jjmalhotra@users.noreply.github.com>
2022-03-02 20:42:39 +0530

    Books library

---
+++ src/books-record.js
@@ -0,0 +1,144 @@
+//CREATE ARRAY OF BOOKS
+
+export const books = [
+  {
+      id:1,
+      img: 'https://m.media-amazon.com/images/I/81219ePreML._AC_UY218_.jpg',
+      title: "Mistress of the Game",
+      author: "Sidney Sheldon",
+      reviews: '807'
+  },
+  {
+      id:2,
+      img: 'https://images-na.ssl-images-amazon.com/images/I/41k5ka3zAML._AC_SX184_.jpg',
+      title: "The Unsinkable Greta James",
+      author: "Jennifer E. Smith",
+      reviews: '3,000'
+  },
+  {
+    id:3,
+    img: 'https://m.media-amazon.com/images/I/61rq+xSrFtS._AC_UY218_.jpg',
+    title: "The Complete Sherlock Holmes ",
+    author: "Sir Arthur Conan Doyle",
+    reviews: '3,753'
+  },
+  {
+    id:4,
+    img:'https://m.media-amazon.com/images/I/71aFt4+OTOL._AC_UY218_.jpg',
+    title: 'The Alchemist',
+    author:'Paulo Coelho',
+    reviews: '70,020'
+  },
+  {
+    id:5,
+    img:'https://m.media-amazon.com/images/I/81l3rZK4lnL._AC_UY218_.jpg',
+    title:'Ikigai: The Japanese secret to a long and happy life',
+    author:'Héctor García and Francesc Miralles',
+    reviews:'29,815'
+  },
+  {
+    id:6,
+    img:'https://m.media-amazon.com/images/I/71dNsRuYL7L._AC_UY218_.jpg',
+    title: 'You Only Live Once: One for Passion Two for Love Three for Friendship',
+    author:'Stuti Changle',
+    reviews: '2,210'
+  },
+  {
+    id:7,
+    img:'https://m.media-amazon.com/images/I/71g2ednj0JL._AC_UY218_.jpg',
+    title: 'The Psychology of Money',
+    author:'Morgan Housel',
+    reviews: '27,793'
+  },
+  {
+    id:8,
+    img:'https://m.media-amazon.com/images/I/81fjvCmxNWL._AC_UY218_.jpg',
+    title: 'The Wife Upstairs',
+    author:'Rachel Hawkins ',
+    reviews: '70,0205,567'
+  },
+  {
+    id:9,
+    img:'https://m.media-amazon.com/images/I/81X0yPyjttL._AC_UY218_.jpg',
+    title: 'The One Small Mistake',
+    author:'Dandy Smith ',
+    reviews: '941'
+  },
+  {
+    id:10,
+    img:'https://m.media-amazon.com/images/I/71aFt4+OTOL._AC_UY218_.jpg',
+    title: 'The Power of A Positive Attitude: Your Road To Success',
+    author:'Roger Fritz',
+    reviews: '1,532'
+  },
+  {
+    id:11,
+    img:'https://m.media-amazon.com/images/I/71pjaEBslxL._AC_UY218_.jpg',
+    title: 'The Archer',
+    author:'Paulo Coelho',
+    reviews: '3,926'
+  },
+  {
+    id:12,
+    img:'https://m.media-amazon.com/images/I/81+x6eHkSdS._AC_UY218_.jpg',
+    title: 'Wuthering Heights',
+    author:'The AlchemistEmily Brontë ',
+    reviews: '15,020'
+  },
+  {
+    id:13,
+    img:'https://m.media-amazon.com/images/I/41wKll7ITyL._AC_UY218_.jpg',
+    title: 'My Life in Full: Work, Family, and our Future',
+    author:'Indra K. Nooyi',
+    reviews: '1,679'
+  },
+  {
+    id:14,
+    img:'https://m.media-amazon.com/images/I/71xydFcbc2L._AC_UY218_.jpg',
+    title: 'How to Win Friends and Influence People',
+    author:'Dale Carnegie ',
+    reviews: '56,898'
+  },
+  {
+    id:15,
+    img:'https://m.media-amazon.com/images/I/815Hgt0wjeL._AC_UY218_.jpg',
+    title: 'The Perfect Us',
+    author:'Durjoy Datta',
+    reviews: '2,133'
+  },
+  {
+    id:16,
+    img:'https://m.media-amazon.com/images/I/41wKll7ITyL._AC_UY218_.jpg',
+    title: 'Who Will Cry When You Die',
+    author:'Robin Sharma',
+    reviews: '13,020'
+  },
+  {
+    id:17,
+    img:'https://m.media-amazon.com/images/I/81tEgsxpNZS._AC_UY218_.jpg',
+    title: 'As a Man Thinketh',
+    author:'James Alen',
+    reviews: '7,120'
+  },
+  {
+    id:18,
+    img:'https://m.media-amazon.com/images/I/91ZXbuNExRL._AC_UY218_.jpg',
+    title: 'The Complete Novels of Sherlock Holmes ',
+    author:'Sir Arthur Conan Doyle ',
+    reviews: '407'
+  },
+  {
+    id:19,
+    img:'https://m.media-amazon.com/images/I/71UwSHSZRnS._AC_UY218_.jpg',
+    title: 'The Power of your Subconscious Mind',
+    author:'Joseph Murphy',
+    reviews: '49,129'
+  },
+  {
+    id:20,
+    img:'https://m.media-amazon.com/images/I/81N7FmJhbhL._AC_UY218_.jpg',
+    title: 'Life\'s Amazing Secrets',
+    author:'Gaur Gopal Das',
+    reviews: '14,327'
+  }
+]
--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'; //For Render
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {books} from './books-record';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Header = () =>{
+  return(
+    <>
+    <header className='header'>
+    Books Gallery
+    </header>
+    <BookList/>
+    <Footer/>
+    </>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Create the component for the list of books
+const BookList = ()=>{
+  return(
+    <section className='book-list'> 
+      {
+        books.map((book)=>{
+          const{img, title, author, reviews} = book;
+          return <Book book={book} key={book.id}/>
+        })
+      }
+    </section>
+  )
+}
+
+//Create the component for the book tag
+const Book = (props)=>{
+  const {img, title, author, reviews}=props.book;
+  return(
+    <article className='book'>
+      <img src={img} alt="Book"/>
+      <h4>{title}</h4>
+      <h5>by {author}</h5>
+      <h5>Reviews: {reviews}<sup><span class="color-star">&#9733;</span></sup></h5>
+    </article>
+    
+  )
+}
+
+
+const Footer = () =>{
+  return(
+    <>
+    <footer className='footer'>
+    &copy; AshGallery
+    </footer>
+    </>
+  )
+}
+
+//Render the Books records
+ReactDOM.render(<Header/>, document.getElementById('root')); 
